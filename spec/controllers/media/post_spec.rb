4580f45df79aaee32f172317666a3f2c7e9ad493
Peter Bengtson <peter@peterbengtson.com>
2013-08-14 12:52:57 +0200

    Updated to the ocean-rails v1.15.3 gem.

--- spec/controllers/media/post_spec.rb
+++ spec/controllers/media/post_spec.rb
@@ -49,7 +49,7 @@ describe MediaController do
       post :create, @args
       response.status.should == 422
       response.content_type.should == "application/json"
-      JSON.parse(response.body).should == {"_api_error" => ["Medium already exists"]}
+      JSON.parse(response.body).should == {"_api_error" => ["Resource not unique"]}
     end
 
     it "should return a 422 when there are validation errors" do
