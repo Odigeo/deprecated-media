fde77266871367e7038f15de96a1fb21b22246e2
Peter Bengtson <peter@peterbengtson.com>
2014-09-03 05:06:21 +0200

    Filtering password, payload, and credentials from the log.

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module Media
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :payload]
+    config.filter_parameters += [:password, :payload, :credentials]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
